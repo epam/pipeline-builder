4264ab55da0012e77dc31fccc1e51bb02700923a
Daniil Savchuk <daniil_savchuk@epam.com>
2017-04-03 17:04:54 +0300

    Added tests for new functions and classes

---
+++ test/model/GroupTest.js
@@ -0,0 +1,55 @@
+import { expect } from 'chai';
+
+import Group from '../../src/model/Group';
+import Action from '../../src/model/Action';
+
+describe('model/Group', () => {
+
+  const name = 'foo';
+  const type = 'bar';
+  const config = {
+    i: { p1: { type: 'int' }, p2: { type: 'string' } },
+    o: { q1: { type: 'file' }, q2: { type: 'string' } },
+  };
+
+  describe('constructor', () => {
+
+    it('requires a name', () => {
+      expect(() => new Group()).to.throw(Error);
+      expect(() => new Group(name)).to.not.throw(Error);
+      expect(new Group(name).name).to.equal(name);
+    });
+
+    it('allows empty configuration', () => {
+      const group = new Group(name);
+      expect(group).to.contain.all.keys(['name', 'i', 'o', 'action', 'children', 'parent']);
+      expect(group.i).to.be.empty;
+      expect(group.o).to.be.empty;
+      expect(group.action).to.be.an.instanceOf(Action, 'group.action');
+      expect(group.children).to.be.empty;
+      expect(group.parent).to.be.null;
+    });
+
+    it('requires a type', () => {
+      expect(() => new Group()).to.throw(Error);
+      expect(() => new Group(name, type)).to.not.throw(Error);
+      expect(new Group(name, type).type).to.equal(type);
+    });
+
+    it('throws error when try to create with ports', () => {
+      expect(() => new Group(name, type, config)).to.throw(Error);
+    });
+
+    it('throws error when try to create with action', () => {
+      expect(() => new Group(name, type, new Action(name, config))).to.throw(Error);
+    });
+  });
+
+  describe('_onActionChanged', () => {
+
+    it('doing nothing', () => {
+      const group = new Group(name);
+      expect(group._onActionChanged()).to.be.empty;
+    });
+  });
+});
