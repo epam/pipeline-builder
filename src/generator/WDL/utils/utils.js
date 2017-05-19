c86b9a195b0e34f4de90278c90f7690556c049d8
Daniil Savchuk <daniil_savchuk@epam.com>
2017-05-19 16:08:23 +0300

    Added string escaping for default mappings

--- src/generator/WDL/utils/utils.js
+++ src/generator/WDL/utils/utils.js
@@ -1,8 +1,28 @@
 import _ from 'lodash';
 import * as constants from '../constants';
 
+export function escapeWDLString(src) {
+  const escapes = {
+    '\\\\': '\\\\',
+    '\\\\0': '\\\\0',
+    '\\\\n': '\\\\n',
+    '\\\\r': '\\\\r',
+    '\\\\b': '\\\\b',
+    '\\\\t': '\\\\t',
+    '\\\\f': '\\\\f',
+    '\\\\a': '\\\\a',
+    '\\\\v': '\\\\v'
+  };
+
+  _.forEach(escapes, (val, key) => {
+    src = src.replace(new RegExp(key, 'g'), escapes[key]);
+  });
+
+  return src;
+}
+
 export function buildDeclarations(declarations, settings) { // eslint-disable-line import/prefer-default-export
-  const buildDeclarationValue = value => ` ${constants.EQ} ${value.toString()}`;
+  const buildDeclarationValue = value => ` ${constants.EQ} ${escapeWDLString(value.toString())}`;
 
   const EOL = settings.getValue('style.eol');
   const SCOPE_INDENT = settings.getValue('style.scope_indent');
