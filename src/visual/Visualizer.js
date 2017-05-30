c14b8bcfc8a4dfc4f1bef4083c7b0c92716e8799
Alexey Terentjev <Aleksei_Terentev@epam.com>
2017-05-30 19:33:31 +0300

    Add forced updated on ports toggle

--- src/visual/Visualizer.js
+++ src/visual/Visualizer.js
@@ -298,6 +298,7 @@ export default class Visualizer {
       _.forOwn(child.step.i, (port, name) => child.togglePort(true, name, value));
       _.forOwn(child.step.o, (port, name) => child.togglePort(false, name, value));
     });
+    this._update();
   }
 
   _loopPorts(ports, source, cellsToAdd, isEnabled) {
