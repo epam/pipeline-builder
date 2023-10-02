f120fb48cefe9cea70ec533fffdef20d6959a4ea
Aleksandr Gorodetskii <41908792+AleksandrGorodetskii@users.noreply.github.com>
2023-10-02 12:17:15 +0300

    Pipeline builder 1.0 alpha improvements (#64)
    * pipeline builder: highlight inputs based on value instead of connections
    
    * pipeline builder: task outputs can reference previous outputs of the same task
    
    * pipeline builder: task outputs can reference previous outputs of the same task - remove parameter order checks

--- src/model/expression/index.ts
+++ src/model/expression/index.ts
@@ -348,16 +348,30 @@ abstract class Expression<T extends TExpressionTypes = TExpressionTypes>
       // Possible connections could be found among
       // task inputs and declarations
       const inputs = currentAction.getActionInputs();
+      const outputs = currentAction.getActionOutputs();
       const declarations = currentAction.getActionDeclarations();
+      const currentParameter = currentAction.getParameterByUUID(this.uuid);
       const list: IParameter[] = []
         .concat(declarations)
         .concat(inputs)
         .filter((d) => (d as IExpression) !== this);
       const getInboundConnectionForDependency = (dependency: string) => list
         .find((o) => this.parameterMatchesDependency(o, dependency));
+      const getOutboundConnectionForDependency = (dependency: string) => {
+        // Outputs can reference another outputs in the same block for a Task
+        if (
+          currentParameter.isOutput
+          && dependency !== currentParameter.name
+        ) {
+          return outputs
+            .find((o) => this.parameterMatchesDependency(o, dependency));
+        }
+        return undefined;
+      };
       notResolvedDependencies.forEach((dependency) => {
         // eslint-disable-next-line no-param-reassign
-        dependency.resolved = getInboundConnectionForDependency(dependency.dependency);
+        dependency.resolved = getInboundConnectionForDependency(dependency.dependency)
+         || getOutboundConnectionForDependency(dependency.dependency);
       });
     }
     return this.dependencies
